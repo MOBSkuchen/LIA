42a8001b113e186678a12afb1e0f4e165a0c3f9d
MOBSkuchen <mobskuchen@gmail.com>
2024-07-17 09:05:59 +0200

    Operation enums for Math operations and cmpOps

---
+++ LIA/Operations.cs
@@ -0,0 +1,22 @@
+﻿namespace LIA;
+
+public enum CmpOps
+{
+    GreaterThan,
+    GreaterThanEquals,
+    LesserThan,
+    LesserThanEquals,
+    Equals,
+    IsFalse,
+    IsTrue
+}
+
+public enum MathOps
+{
+    Add,
+    Sub,
+    Mul,
+    Div,
+    Rem,
+    Xor
+}
