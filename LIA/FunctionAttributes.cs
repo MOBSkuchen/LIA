f7e67d68d688b587612e86b3868db69fa6d0d31a
MOBSkuchen <mobskuchen@gmail.com>
2024-07-17 09:06:19 +0200

    FunctionAttributes

---
+++ LIA/FunctionAttributes.cs
@@ -0,0 +1,37 @@
+﻿namespace LIA;
+
+public struct FunctionAttributes(string ns, string name, 
+    bool isStatic, bool isPublic, Type type, List<(string, Type)>? args, string? library = null)
+{
+    public string Namespace = ns;
+    public string? Library = library;
+    public bool IsStatic = isStatic;
+    public bool IsPublic = isPublic;
+    public string Name = name;
+    public Type Type = type;
+    public List<(string, Type)>? Arguments = args;
+
+    public string Generate()
+    {
+        List<string> stack = new List<string>();
+        
+        if (IsPublic) stack.Add("public");
+        else stack.Add("private");
+        if (IsStatic) stack.Add("static");
+        
+        stack.Add(Type.Get());
+        stack.Add(Name);
+        stack.Add("(");
+
+        if (Arguments != null)
+        {
+            foreach (var valueTuple in Arguments)
+            {
+                stack.Add($"{valueTuple.Item2.Get()} {valueTuple.Item1},");
+            }
+        }
+
+        stack.Add(")");
+        return string.Join(" ", stack);
+    }
+}
