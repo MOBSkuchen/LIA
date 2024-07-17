a3d01b82c627298c129335e2a5095d7a7d093ee7
MOBSkuchen <mobskuchen@gmail.com>
2024-07-17 09:06:07 +0200

    Types

---
+++ LIA/Types.cs
@@ -0,0 +1,13 @@
+﻿namespace LIA;
+
+public class Type
+{
+    private string _literal;
+    public Type(string literal, bool isRef = false, bool goesOut = false)
+    {
+        _literal = (goesOut ? "[out] " : "") + _literal;
+        _literal = isRef ? literal + "&" : literal;
+    }
+
+    public string Get() => _literal;
+}
