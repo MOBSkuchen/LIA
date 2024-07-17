40caceee63d4a9ca1373f19304597bdb5451e367
MOBSkuchen <mobskuchen@gmail.com>
2024-07-17 17:33:38 +0200

    Utils

---
+++ LIA/Utils.cs
@@ -0,0 +1,25 @@
+﻿namespace LIA;
+
+public class Utils
+{
+    public static char Quote()
+    {
+        return '"';
+    }
+    public static string FormatLines(string lns)
+    {
+        List<string> stack = new List<string>();
+        
+        foreach (var ln in lns.Split("\n"))
+        {
+            stack.Add($"  {ln}");
+        }
+
+        return string.Join("\n", stack);
+    }
+
+    public static void WriteStringTo(string path, string text)
+    {
+        File.WriteAllText(path, text);
+    }
+}
