33f9fc9df63e0ac53701361620288ce7a28875f2
MOBSkuchen <mobskuchen@gmail.com>
2024-07-17 17:33:07 +0200

    ClassAttributes

---
+++ LIA/ClassAttributes.cs
@@ -0,0 +1,20 @@
+﻿namespace LIA;
+
+public struct ClassAttributes(string ns, bool isPublic, string name)
+{
+    public string NameSpace = ns;
+    public string Name = name;
+    public bool IsPublic = isPublic;
+    
+    public string Generate()
+    {
+        List<string> stack = new List<string>();
+        
+        if (IsPublic) stack.Add("public");
+        else stack.Add("private");
+        
+        stack.Add(Name);
+
+        return string.Join(" ", stack);
+    }
+}
