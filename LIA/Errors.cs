13797e319eaf94d37b2f1fda7b8d061ecf48882b
MOBSkuchen <mobskuchen@gmail.com>
2024-07-24 15:34:00 +0200

    Change error position representation

--- LIA/Errors.cs
+++ LIA/Errors.cs
@@ -21,8 +21,8 @@ public class Errors
         
         string position = "line ";
         position += startPos.Item1 == endPos.Item1 ? $"{startPos.Item1}" : $"{startPos.Item1}-{endPos.Item1}";
-        position += ";";
-        position += startPos.Item2 == endPos.Item2 ? $"{startPos.Item2}" : $"{startPos.Item2}:{endPos.Item2}";
+        position += ":";
+        position += startPos.Item2 == endPos.Item2 ? $"{startPos.Item2}" : $"{startPos.Item2}-{endPos.Item2}";
         
         Console.WriteLine($"In file " +
                           $"'{ConsoleLib.As(codeLocation.CodeFile.Filepath, ConsoleLib.TextBold, ConsoleLib.FgCyan)}'" +
@@ -36,8 +36,8 @@ public class Errors
         $"{ConsoleLib.MakeColor(ConsoleLib.TextUnderline, ConsoleLib.FgRed)}" +
         $"{lines[startPos.Item1].Substring(startPos.Item2, endPos.Item2 - startPos.Item2)}" + ConsoleLib.ResetColor() +
         $"{ConsoleLib.MakeColor(ConsoleLib.TextItalic, ConsoleLib.FgMagenta)}{lines[startPos.Item1].Substring(endPos.Item2, lines[startPos.Item1].Length - endPos.Item2)}", ConsoleLib.TextItalic, ConsoleLib.FgMagenta));
-        if (startPos.Item1 != (lines.Count - 1)) Console.WriteLine($"{startPos.Item1+1} | {lines[startPos.Item1+1]}"); 
-        
+        if (startPos.Item1 != (lines.Count - 1)) Console.WriteLine($"{startPos.Item1+1} | {lines[startPos.Item1+1]}");
+        throw new Exception();
         Exit(errNum);
     }
 
