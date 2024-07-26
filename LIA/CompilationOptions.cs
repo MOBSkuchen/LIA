e48dfd65d8d631e4c3464e2c52db1df66f00255e
MOBSkuchen <mobskuchen@gmail.com>
2024-07-26 21:21:22 +0200

    Create Compiler options

---
+++ LIA/CompilationOptions.cs
@@ -0,0 +1,31 @@
+﻿namespace LIA;
+
+public class CompilationOptions
+{
+    // Disable Warnings
+    public bool DisableWarningMainNotDefined;
+    public bool DisableWarningUselessCode;
+    public bool DisableWarningUnreachableCode;
+    // Trim
+    public bool TrimUnreachableCode;
+    public bool TrimUselessCode;
+
+    public CompilationOptions()
+    {
+        SetAllWarnings(false);
+        SetAllTrim(false);
+    }
+
+    public void SetAllWarnings(bool state)
+    {
+        DisableWarningMainNotDefined = state;
+        DisableWarningUselessCode = state;
+        DisableWarningUnreachableCode = state;
+    }
+
+    public void SetAllTrim(bool state)
+    {
+        TrimUnreachableCode = state;
+        TrimUselessCode = state;
+    }
+}
